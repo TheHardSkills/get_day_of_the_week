2b4f90bb62412c9eb7a1d0e49d998cda101f21d9
YuliaOkushko <juli.okushko@gmail.com>
2020-05-13 17:03:47 +0300

    new logic

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # get_day_of_the_week
-Search for an alien day of the week
+Search for an alien day of the week.
 
-For start run "index.html".
+To start, run file "index.html".
+
+In function: get_x_1(), get_x_1(), get_x_1() - accrued time intervals.
+x1 - count of days from constant date to new year;
+x2 - count of full year from const date to enter date;
+x3 - count of days from new year to enter date.
--- main.js
+++ main.js
@@ -1,97 +1,144 @@
-const baseFullDate = ["24.08.1", "Tuesday"];
-
-const parseDate = (date) => {
-    const dateArray = date.split('.');
-    let arr = [];
-    for (let i = 0; i < dateArray.length; i++) {
-        arr.push(Number(dateArray[i]));
-    }
-    return arr;
-}
-
-var sendButton = document.getElementById('sendButton');
+const constantFullDate = ["24.08.1", "Tuesday"];
+const sendButton = document.getElementById('sendButton');
 
 sendButton.onclick = function () {
-    let val = document.getElementById('enterYear').value;
-    const parseBaseDate = parseDate(baseFullDate[0]);
-    const parseEnterValue = parseDate(val);
+    const constantFullDate = ["24.08.1", "Tuesday"];
 
-    if (parseEnterValue.length === 3) {
-        if ((parseEnterValue[0] > 0 && parseEnterValue[0] <= 30) && (parseEnterValue[1] > 0 && parseEnterValue[1] <= 12)) {
-            document.getElementById('str').innerHTML = "Day of the week: " + findTheDayOfTheWeekOnChronos(parseBaseDate, parseEnterValue);
+    const enterDate = document.getElementById('enterYear').value;
+    const constDateForProcessing = getParsedDate(constantFullDate[0]);
+    const enterDateForProcessing = getParsedDate(enterDate);
+
+    if (enterDateForProcessing.length === 3) {
+        if ((enterDateForProcessing[0] > 0 && enterDateForProcessing[0] <= 30) && (enterDateForProcessing[1] > 0 && enterDateForProcessing[1] <= 12)) {
+            document.getElementById('str').innerHTML = "Day of the week: " + getDayOfTheWeek(getIntermediateCalculations(enterDate));
+            (constDateForProcessing, enterDateForProcessing);
+        }
+        else if (enterDateForProcessing[0] === 31 && enterDateForProcessing[1] === 2) {
+            if (getCountOfDaysInYear(enterDateForProcessing[2]) === 361) {
+                document.getElementById('str').innerHTML = "Day of the week: " + getDayOfTheWeek(getIntermediateCalculations(enterDate));
+            } else alert("Input Error");
         } else alert("Input Error");
     } else alert("Input Error");
-
 };
 
-const findLeapYearOnChronos = (year) => {
-    let leapYear = 0;
+const getCountOfDaysInYear = (year) => {
+    const normalYear = 360;
+    const leapYear = 361;
+    let countOfDaysInYear = normalYear;
     if (year % 5 === 0) {
         if (year % 100 === 0) {
             if (year % 500 === 0) {
-                leapYear = year;
+                countOfDaysInYear = leapYear;
             }
         }
-        else leapYear = year;
-    }
-    return leapYear;
-}
-const findLeapYearsElapsedFromEnteredDate = (baseDate, targetDate) => {
-    const targetDateYear = targetDate[2];
-    const baseDateYear = baseDate[2];
-
-
-    let listOfYearsBefore = [];
-    for (let i = (baseDateYear - 0); i < targetDateYear; i++) {
-        listOfYearsBefore.push(i);
-    }
-
-    let listOfLeapYear = [];
-    for (let i = 0; i < listOfYearsBefore.length; i++) {
-        let res = findLeapYearOnChronos(listOfYearsBefore[i]);
-        if (res !== 0) {
-            listOfLeapYear.push(res);
-        }
-    }
-    return listOfLeapYear;
-}
-
-const findTheDayOfTheWeekOnChronos = (baseDate, targetDate) => {
-    const daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    const countOfLeapDays = 361;
-    const countOfNormalDays = 360;
-
-    const baseDateDay = baseFullDate[1];
-    let baseDateDayIndex = 0;
-    for (let i = 0; i < daysOfTheWeek.length; i++) {
-        if (baseDateDay === daysOfTheWeek[i]) {
-            baseDateDayIndex = i;
-        }
+        else countOfDaysInYear = leapYear;
     };
-    console.log(baseDateDayIndex)
-    let countOfYearsPassed = baseDate[2] - targetDate[2];
-    const a = -1
-    if (countOfYearsPassed < 0) { countOfYearsPassed = countOfYearsPassed * a }
-    const leapYearsPast = findLeapYearsElapsedFromEnteredDate(baseDate, targetDate);
-    const countOfLeapYears = leapYearsPast.length;
+    return countOfDaysInYear;
+};
 
-    const countOfNormalYears = countOfYearsPassed - countOfLeapYears;
-    const countOfDaysInTheCurrentYear = (targetDate[1] * 30) + targetDate[0];
-    const countOfDaysInTheBaseYear = (baseDate[1] * 30) + baseDate[0];
+const getParsedDate = (date) => {
+    const stringFormatDateInArray = date.split('.');
+    let dateInArray = [];
+    for (let i = 0; i < stringFormatDateInArray.length; i++) {
+        dateInArray.push(Number(stringFormatDateInArray[i]));
+    };
+    return dateInArray;
+};
 
-    const sumOfDaysAfterBaseDate = (countOfLeapYears * countOfLeapDays) + (countOfNormalYears * countOfNormalDays) + (countOfDaysInTheCurrentYear - countOfDaysInTheBaseYear);
-    const targetDateOfTheWeek = (sumOfDaysAfterBaseDate % 7);
+const get_x_1 = (constDate, enterDate) => {
+    //count of days from constant date to new year
+    const enterDateForProcessing = enterDate;
+    const constDateForProcessing = constDate;
+
+    const countOfDays = constDateForProcessing[0] + (constDateForProcessing[1] * 30);
+    const countOfDaysInYear = getCountOfDaysInYear(constDateForProcessing[2]);
+    let countOfDaysToNewYear = countOfDaysInYear - countOfDays;
+
+    const whatIsThisYear = getCountOfDaysInYear(enterDateForProcessing[2]);
+    if (whatIsThisYear === 361 && enterDateForProcessing[1] > 2) { //if the year is a leap year, 2 - February number
+        countOfDaysToNewYear += 1;
+    };
+    if (enterDateForProcessing[2] === constDateForProcessing[2]) {
+        countOfDaysToNewYear = 0;
+    };
+    return countOfDaysToNewYear;
+};
+
+const get_x_2 = (constDate, enterDate) => {
+    //count of full year from const date to enter date
+    const enterDateForProcessing = enterDate;
+    const constDateForProcessing = constDate;
+
+    let countOfDaysFromTheEnteredToConstantDate = [];
+    for (let i = constDateForProcessing[2] + 1; i <= enterDateForProcessing[2] - 1; i++) {
+        const countOfDaysInTheYear = getCountOfDaysInYear(i);
+        countOfDaysFromTheEnteredToConstantDate.push(countOfDaysInTheYear);
+    };
+
+    let countOfDaysFromEnteredToConstantDate = 0;
+    for (let i = 0; i < countOfDaysFromTheEnteredToConstantDate.length; i++) {
+        countOfDaysFromEnteredToConstantDate += countOfDaysFromTheEnteredToConstantDate[i];
+    };
+    return countOfDaysFromEnteredToConstantDate;
+};
+
+const get_x_3 = (enterDate) => {
+    //count of days from new year to enter date
+    const enterDateForProcessing = enterDate;
+
+    const countOfDaysFromNewYearToEnterDate = enterDateForProcessing[0] + (enterDateForProcessing[1] * 30);
+    const whatIsThisYear = getCountOfDaysInYear(enterDateForProcessing[2]);
+    if (whatIsThisYear === 361 && enterDateForProcessing[1] > 2) { //if the year is a leap year, 2 - February number
+        countOfDaysFromNewYearToEnterDate += 1;
+    };
+    return countOfDaysFromNewYearToEnterDate;
+};
+
+const getConstantDataInDays = (enterDateForProcessing) => {
+    const constDateForProcessing = getParsedDate(constantFullDate[0]);
+    const constantDataInDays = constDateForProcessing[0] + (constDateForProcessing[1] * 30);
+    const whatIsThisYear = getCountOfDaysInYear(enterDateForProcessing[2]);
+    if (whatIsThisYear === 361 && constDateForProcessing[1] > 2) { //if the year is a leap year, 2 - February number
+        constantDataInDays += 1;
+    };
+    return constantDataInDays;
+};
+
+const getIntermediateCalculations = (enterDate) => {
+    const constDateForProcessing = getParsedDate(constantFullDate[0]);
+    const enterDateForProcessing = getParsedDate(enterDate);
+
+    const getCountOfDays = get_x_1(constDateForProcessing, enterDateForProcessing) + get_x_2(constDateForProcessing, enterDateForProcessing) + get_x_3(enterDateForProcessing);
+
+    let countOfDaysPassedFromConstantToEnteredDate = getCountOfDays;
+    if (enterDateForProcessing[2] === constDateForProcessing[2]) {
+        countOfDaysPassedFromConstantToEnteredDate = getCountOfDays - getConstantDataInDays(enterDateForProcessing);
+    };
+    let getCountsOfDaysInNotFullWeek = getCountOfDays % 7;
+    if (enterDateForProcessing[2] === constDateForProcessing[2]) {
+        getCountsOfDaysInNotFullWeek = countOfDaysPassedFromConstantToEnteredDate % 7;
+    };
+    return getCountsOfDaysInNotFullWeek;
+};
 
 
-    const dayOfTheWeekIndex = baseDateDayIndex + targetDateOfTheWeek;
-    let needIndex = null;
-    let trueDayOfTheWeek = null;
-    if (dayOfTheWeekIndex >= daysOfTheWeek.length) {
-        needIndex = dayOfTheWeekIndex - daysOfTheWeek.length;
-        trueDayOfTheWeek = daysOfTheWeek[needIndex];
-    }
-    else {
-        trueDayOfTheWeek = daysOfTheWeek[dayOfTheWeekIndex];
-    }
-    return trueDayOfTheWeek;
-}
+const getDayOfTheWeek = (getCountsOfDaysInNotFullWeek) => {
+    const daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const constDayOfTheWeekForProcessing = constantFullDate[1];
+    let indexForConstDayOfTheWeek = 0;
+    for (let i = 0; i < daysOfTheWeek.length; i++) {
+        if (constDayOfTheWeekForProcessing === daysOfTheWeek[i]) {
+            indexForConstDayOfTheWeek = i;
+        };
+    };
+    const res = indexForConstDayOfTheWeek + getCountsOfDaysInNotFullWeek;
+    let indexOfTheEnteredDayOfTheWeek = res % 7;
+
+    if (indexOfTheEnteredDayOfTheWeek < 0) {
+        indexOfTheEnteredDayOfTheWeek = daysOfTheWeek.length + indexOfTheEnteredDayOfTheWeek;
+    };
+    const dayOfTheWeek = daysOfTheWeek[indexOfTheEnteredDayOfTheWeek];
+    return dayOfTheWeek;
+};
+
+
